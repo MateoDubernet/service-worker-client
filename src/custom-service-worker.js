775be51f92d144aaf35fa30eb6f495fd0c1874be
MateoDubernet <107269857+MateoDubernet@users.noreply.github.com>
2025-09-10 00:36:50 +0200

    Update custom-service-worker.js

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -2,13 +2,6 @@ self.addEventListener('install', () => {
     self.skipWaiting();
 })
 
-// self.addEventListener('activate', (event) => { 
-//     console.log('[Service Worker] Activation'); 
-//     event.waitUntil(
-//         self.clients.claim()
-//     );
-// });
-
 self.addEventListener('push', (event) => {
     const data = event.data ? event.data.json() : {};
     console.log('********************************** ZZZZZZZZZ');
